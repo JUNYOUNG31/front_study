9a2ba4dc28aad8db49f9a3f4701d7a29d4537768
JUNYOUNG31 <roywnsdud31@gmail.com>
2022-02-26 00:58:03 +0900

    ReactMemo: Component Route

--- react/ReactMemo/ClientApp/src/App.js
+++ react/ReactMemo/ClientApp/src/App.js
@@ -4,6 +4,10 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { About } from './components/About';
+import { Contact } from './components/Contact';
+import ReactFragmentDemo from './components/Samples/ReactFragmentDemo';
+
 
 import './custom.css'
 
@@ -16,6 +20,9 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
+        <Route path='/about' component={About}/>
+        <Route path='/contact' component={Contact}/>
+        <Route path='/ReactFragmentDemo' component={ReactFragmentDemo}/>
       </Layout>
     );
   }
