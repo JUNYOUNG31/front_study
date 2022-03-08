255bf9356748171b00a38d81ea4ad1e41d47a199
JUNYOUNG31 <roywnsdud31@gmail.com>
2022-03-08 18:51:30 +0900

    nomadcoder: Movie App

---
+++ react/nomadcoders/practice-movie-app/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+function App() {
+  return <Router>
+    <Routes>
+      <Route path="/movie" element={<Detail/>}></Route>
+      <Route path="/" element={< Home />}></Route>
+      {/* <Route path="/hello"><h1>Hello</h1></Route> */}
+    </Routes>
+  </Router>
+}
+
+export default App;
