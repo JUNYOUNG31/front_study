1dfd58a23b2022d44c87bf5e12a407eeb2f9d781
JUNYOUNG31 <roywnsdud31@gmail.com>
2022-12-02 22:13:34 +0900

    redux study6

--- react/nomadcoders/redux/vanila-redux/src/store.js
+++ react/nomadcoders/redux/vanila-redux/src/store.js
@@ -1,37 +1,37 @@
 import { createStore } from "redux";
 
-const ADD = "ADD"
-const DELETE = "DELETE"
+const ADD = "ADD";
+const DELETE = "DELETE";
 
-export const addToDo = text => {
-    return {    
+const addToDo = text => {
+    return {
         type: ADD,
         text
     };
 };
 
-export const deleteToDo = id => {
+const deleteToDo = id => {
     return {
         type: DELETE,
         id
+    };
 };
-};
-
 const reducer = (state = [], action) => {
-    switch ( action.type) {
+    switch (action.type) {
         case ADD:
-            return [{ text: action.text, id: Date.now() }, ...state];
+        return [{ text: action.text, id: Date.now() }, ...state];
         case DELETE:
-            return state.filter(toDo => toDo.id !== action.id);
+        return state.filter(toDo => toDo !== action.id);
         default:
-            return state;
+        return state;
     }
-}
+};
 
+const store = createStore(reducer);
 
-const store = createStore(reducer)
-
-
-store.subscribe()
+export const actionCreators = {
+    addToDo,
+    deleteToDo
+};
 
 export default store;
