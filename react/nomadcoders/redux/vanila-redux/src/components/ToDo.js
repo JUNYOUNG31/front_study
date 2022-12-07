f6cbdc8c1d4b01db72b90b306de28cea48bdebe5
JUNYOUNG31 <roywnsdud31@gmail.com>
2022-12-07 21:33:27 +0800

    redux study8

--- react/nomadcoders/redux/vanila-redux/src/components/ToDo.js
+++ react/nomadcoders/redux/vanila-redux/src/components/ToDo.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
 
-function ToDo({ text, onBtnClick }) {
+function ToDo({ text, onBtnClick, id }) {
     return (
         <li>
-        {text} <button onClick={onBtnClick}>DEL</button>
+            <Link to={`/${id}`}>
+            {text} <button onClick={onBtnClick}>DEL</button>
+            </Link>
         </li>
     );
-    }
+}
     
-    function mapDispatchToProps(dispatch, ownProps) {
+function mapDispatchToProps(dispatch, ownProps) {
     return {
         onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id))
     };
